c6ae517b84120414d8354bda80cc099f61dd8b14
Dean Perry <djp42@cantab.net>
2018-09-15 23:47:02 +0100

    Add 16/17 frame scores

--- Screen scrape LR.R
+++ Screen scrape LR.R
@@ -122,9 +122,14 @@ frame_scores_17 <- pmap_dfr(unname(results_17), scrape_match_page)
 frame_scores_18 <- pmap_dfr(unname(results_18), scrape_match_page)
 
 # Combine and filter out BYEs
-frame_scores_total <- rbind(frame_scores_14, frame_scores_15, frame_scores_16,
-                            frame_scores_17, frame_scores_18)
+frame_scores_total <- rbind(frame_scores_14, frame_scores_15
+                            , frame_scores_16
+                            # , frame_scores_17
+                            # , frame_scores_18
+                            )
+frame_scores_total <- frame_scores_total %>%
+  filter(!(is.na(home_score) | is.na(away_score)))
 
 # Write the results to a CSV file for use in the ELO ranking
-write_csv(frame_scores_14, "New-website-frame-scores.csv")
+write_csv(frame_scores_total, "New-website-frame-scores.csv")
 write_csv(results, "New-website-match-scores.csv")
