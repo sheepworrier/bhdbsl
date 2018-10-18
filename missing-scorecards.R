748cb1d4c5f7df287ebbed121d72ebc35d6ffd3f
Dean Perry <djp42@cantab.net>
2018-10-18 15:51:05 +0100

    New script to export the missing scorecard details

---
+++ missing-scorecards.R
@@ -0,0 +1,29 @@
+source("common-functions.R")
+# Read in the match and frame scores from the saved CSVs
+match_scores <-
+  rbind(read_csv("Old-website-match-scores.csv") %>%
+          rename(home_team = `Home Team`, away_team = `Away Team`,
+                 home_score = `Home Score`, away_score = `Away Score`,
+                 season = Season) %>%
+          mutate(division = as.integer(substring(Division, 10, 10)),
+                 fixture_date = as.Date(`Date dd/mm/yyyy`, "%d/%m/%Y")) %>%
+          select(fixture_date, season, division, home_team,
+                 away_team, home_score, away_score),
+        read_csv("New-website-match-scores.csv") %>%
+          mutate(fixture_date = as.Date(fixture_date, "%d/%m/%Y")) %>%
+          select(-URLs))
+frame_scores <- read_csv("Frame-scores.csv") %>%
+  mutate(fixture_date = as.Date(fixture_date))
+# Perform an anti-join to derive missing scorecards
+missing_scorecards <- match_scores %>%
+  anti_join(frame_scores, by = c("fixture_date", "season", "division",
+                                 "home_team", "away_team"))
+# Write output to CSV file
+write_csv(missing_scorecards, "missing-scorecards.csv")
+# Create a cross tab of missing scorecards by division and season
+crosstab <- missing_scorecards %>%
+  count(season, division) %>%
+  rename(missing = n) %>%
+  spread(division, missing)
+colnames(crosstab) <-
+  c("season", paste("Division", colnames(crosstab)[2:ncol(crosstab)]))
