0f4ea99452b59a45ea668b13864f243294a25c63
Dean Perry <djp42@cantab.net>
2018-11-20 12:36:02 +0000

    Fixed some odd bugs.  Not sure why they didn't occur last time...

--- Screen scrape LR (incremental).R
+++ Screen scrape LR (incremental).R
@@ -6,15 +6,15 @@ ref_data <- read_csv("Snooker-Results-pages-per-season.csv")
 # Grab every match result and the link to the match details page
 results_new <- pmap_dfr(unname(ref_data), get_season_division_results)
 results_old <- read_csv("New-website-match-scores.csv",
-           col_types = cols(
-             fixture_date = col_character(),
-             season = col_integer(),
-             division = col_integer(),
-             home_team = col_character(),
-             away_team = col_character(),
-             home_score = col_integer(),
-             away_score = col_integer(),
-             URLs = col_character()))
+                        col_types = cols(
+                          fixture_date = col_character(),
+                          season = col_integer(),
+                          division = col_integer(),
+                          home_team = col_character(),
+                          away_team = col_character(),
+                          home_score = col_integer(),
+                          away_score = col_integer(),
+                          URLs = col_character()))
 # Will definitely scrape any new results, plus any old results that we didn't
 # have frame details for
 # new_results_to_scrape <- setdiff(results_new, results_old)
@@ -23,7 +23,19 @@ new_results_to_scrape <- results_new %>%
                                 "home_team", "away_team", "home_score",
                                 "away_score", "URLs"))
 # Read in the formerly scraped frame scores
-frame_scores_old <- read_csv("New-website-frame-scores.csv")
+frame_scores_old <- read_csv("New-website-frame-scores.csv",
+                             col_types = cols(
+                               fixture_date = col_character(),
+                               season = col_integer(),
+                               division = col_integer(),
+                               home_team = col_character(),
+                               away_team = col_character(),
+                               home_player_id = col_integer(),
+                               home_player_name = col_character(),
+                               home_score = col_integer(),
+                               away_player_id = col_integer(),
+                               away_player_name = col_character(),
+                               away_score = col_integer()))
 # Calculate which old results have no frame scores
 old_results_to_scrape <- results_old %>%
   anti_join(frame_scores_old, by = c("fixture_date", "season", "division",
