061dad9174b68c2686e39edb40c3d1bef05e3a80
Dean Perry <djp42@cantab.net>
2019-02-26 20:14:10 +0000

    Amend script to pick up newly added Premier results rom 17/18

--- 1. Screen scrape LR (incremental).R
+++ 1. Screen scrape LR (incremental).R
@@ -39,10 +39,13 @@ frame_scores_old <- read_csv("New-website-frame-scores.csv",
 # Calculate which old results have no frame scores
 old_results_to_scrape <- results_old %>%
   anti_join(frame_scores_old, by = c("fixture_date", "season", "division",
-                                     "home_team", "away_team"))
+                                     "home_team", "away_team")) %>%
+  filter(season == 17, division == 1)
 # Old result cards will not regularly be entered into the system, so generally
 # just scrape new results for frame scores
-frame_scores_new <- pmap_dfr(unname(new_results_to_scrape), scrape_match_page)
+frame_scores_new <-
+  pmap_dfr(unname(rbind(new_results_to_scrape, old_results_to_scrape)),
+                  scrape_match_page)
 
 # Combine and filter out BYEs
 frame_scores_total <- rbind(frame_scores_old, frame_scores_new)
