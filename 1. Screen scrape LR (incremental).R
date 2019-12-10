e62bdc4b85fdb3fd7d8cef51f4c7bc60fcbd742b
sheepworrier <djp42@cantab.net>
2019-12-10 23:33:24 +0000

    Bug fix - was not saving results from 2014-2018

--- 1. Screen scrape LR (incremental).R
+++ 1. Screen scrape LR (incremental).R
@@ -16,6 +16,12 @@ results_old <- read_csv("New-website-match-scores.csv",
                           home_score = col_integer(),
                           away_score = col_integer(),
                           URLs = col_character()))
+# Update results_new to include any results from result_old that are nor from
+# the current season
+results_new <- results_new %>%
+  bind_rows(results_old %>%
+              filter(!season %in% results_new$season))
+
 # Will definitely scrape any new results, plus any old results that we didn't
 # have frame details for
 # new_results_to_scrape <- setdiff(results_new, results_old)
@@ -41,11 +47,12 @@ frame_scores_old <- read_csv("New-website-frame-scores.csv",
 old_results_to_scrape <- results_old %>%
   anti_join(frame_scores_old, by = c("fixture_date", "season", "division",
                                      "home_team", "away_team")) %>%
-  filter(season == 17, division == 1)
-# Old result cards will not regularly be entered into the system, so generally
-# just scrape new results for frame scores
+  filter(season == 19)
+# Scrape the likely fixtures that will now have match scores
 frame_scores_new <-
-  pmap_dfr(unname(new_results_to_scrape), scrape_match_page)
+  pmap_dfr(unname(new_results_to_scrape %>%
+                    bind_rows(old_results_to_scrape)),
+           scrape_match_page)
 
 # Combine and filter out BYEs
 frame_scores_total <- rbind(frame_scores_old, frame_scores_new)
