0f3f7b08415101649d4802d8cf273c03b0615e8b
sheepworrier <djp42@cantab.net>
2020-02-25 22:12:17 +0000

    Weekly increments

--- 5. Send weekly email.R
+++ 5. Send weekly email.R
@@ -4,8 +4,8 @@ library(readr)
 library(tidyr)
 library(dplyr)
 
-snooker_week <- 21
-billiards_week <- 14
+snooker_week <- 22
+billiards_week <- 15
 # test_to_address <- "djp42@cantab.net"
 email_addresses <- read_csv("team_email_addresses.csv")
 snooker_divisions <- data.frame(division = seq(1, 4),
