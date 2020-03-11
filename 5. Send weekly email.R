10b3baf5fab59910a5dd826c1dc1cd8d25c0086f
sheepworrier <djp42@cantab.net>
2020-03-11 20:37:46 +0000

    Weekly updates

--- 5. Send weekly email.R
+++ 5. Send weekly email.R
@@ -4,7 +4,7 @@ library(readr)
 library(tidyr)
 library(dplyr)
 
-snooker_week <- 23
+snooker_week <- 24
 billiards_week <- 15
 # test_to_address <- "djp42@cantab.net"
 email_addresses <- read_csv("team_email_addresses.csv")
